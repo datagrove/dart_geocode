c41e0235ae2bd7bc01ab7ed62bf2c3c62a2fbc90
r-southworth <rachael.southworth@datagrove.com>
2022-10-01 15:06:18 -0400

    webrequest calculate TMS returning results

--- example/webrequest.dart
+++ example/webrequest.dart
@@ -3,32 +3,34 @@ import 'dart:convert';
 import 'dart:math';
 
 void main() async{
-var decode;
 var url = Uri.parse('https://nominatim.openstreetmap.org/search?street=1270+PEACH+ST&state=CA&postalcode=93401&format=json&limit=2');
 var response = await http.get(url);
 //var body = jsonDecode(response.body);
 //print('Response status: ${response.statusCode}');
 //print('Response body: ${response.body}');
+
+var test = <String, dynamic>{};
+
 if (response.statusCode == 200){
-  decode = jsonDecode(response.body);
-  return decode;
+  var decode = jsonDecode(response.body);
+  test['lat'] = decode[0]['lat'];
+  test['lon'] = decode[0]['lon'];
 };
-print(decode);
-/*
-var lat = decode[0]['lat'];
-var lon = decode[0]['lon'];
-print(lat);
-print(lon);
-*/
-/*
+print(test);
+print(test['lon']);
+print(test['lat']);
+
+
 var zoom = 14;
 var n = 1 << zoom;
-var xtile = n * ((lon+180)/360);
-xtile = xtile.floor();
+double lon = double.parse(test['lon']);
+double lat = double.parse(test['lat']);
+var xcalc = n * ((lon+180)/360);
+var xtile = xcalc.floor();
 var lat_rad = lat*(pi/180);
-var ytile = (n * (1-((log((tan(lat_rad))+(1/(cos(lat_rad))))/log(2))/pi)))/2;
-ytile = ytile.floor();
+var ycalc = (n * (1-((log((tan(lat_rad))+(1/(cos(lat_rad))))/log(2))/pi)))/2;
+var ytile = ycalc.floor();
 
-print('${zoom}, ${ztile}, ${ytile}');
-*/
+print('zoom: ${zoom}, xtile: ${xtile}, ytile: ${ytile}');
+
 }
