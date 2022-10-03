df259144ae20d9b1a674f6416cd831c6feefa7f2
r-southworth <rachael.southworth@datagrove.com>
2022-10-03 10:42:45 -0400

    Broke down Y math - still not working

--- example/webrequest.dart
+++ example/webrequest.dart
@@ -23,14 +23,26 @@ print(test['lat']);
 
 var zoom = 14;
 var n = 1 << zoom;
+print(n);
 double lon = double.parse(test['lon']);
 double lat = double.parse(test['lat']);
 var xcalc = n * ((lon+180)/360);
 var xtile = xcalc.floor();
-var lat_rad = lat*(pi/180);
-var ycalc = (n * (1-((log((tan(lat_rad))+(1/(cos(lat_rad))))/log(2))/pi)))/2;
-var ytile = ycalc.floor();
+var lat_rad = lat*pi/180;
+print(lat_rad);
+var sec_lat = 1/(cos(lat_rad));
+print('sec:${sec_lat}');
+var tan_lat = tan(lat_rad);
+print('tan:${tan_lat}');
+var y = (log(tan_lat + sec_lat))/(log(2));
+print(y);
+var ytrans = (1 - (y/pi))/2;
+print(ytrans);
+var ycalc = n * ytrans;
+print(ycalc);
+///var ycalc = (n * (1-((log((tan(lat_rad))+(1/(cos(lat_rad))))/log(2))/pi)))/2;
+//var ytile = ycalc.floor();
 
-print('zoom: ${zoom}, xtile: ${xtile}, ytile: ${ytile}');
+//print('zoom: ${zoom}, xtile: ${xtile}, ytile: ${ytile}');
 
 }
